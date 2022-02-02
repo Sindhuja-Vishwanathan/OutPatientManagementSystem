91cff2afab2ae93b8bb1e1e773a39ce2b9ed655a
Sindhuja-Vishwanathan <98481280+Sindhuja-Vishwanathan@users.noreply.github.com>
2022-02-03 01:01:26 +0530

    Add files via upload

---
+++ Sushmitha/OutPatientManagementSystem/OutPatientManagementSystem/Models/Serialisation.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Imports System.Xml.Serialization
+
+Public Class Serialisation
+    Public obj
+    Public ds As New DataSet
+    Public Sub XmlDeserialize()
+
+        Dim xmlSerializer As XmlSerializer = New XmlSerializer(ds.GetType)
+        Dim readStream As FileStream = New FileStream("DoctorsDetails.xml", FileMode.Open)
+        ds = CType(xmlSerializer.Deserialize(readStream), DataSet)
+        readStream.Close()
+
+    End Sub
+
+End Class
